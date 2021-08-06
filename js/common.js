c72dbb2610b6d0c54bcb15066aa28f764b4408ba
s.takahata <taka.s@do.enjoy.ne.jp>
2021-08-06 20:15:26 +0900

    hp2test2回目

--- js/common.js
+++ js/common.js
@@ -6,12 +6,15 @@ function sizecheck() {
 }
 
 sizecheck();
-
 $(function () {
-
-  if (w > 769) {
+if (w > 769) {
 } else { 
   $("header .drawer").click(function(){
+  $('body').toggleClass('nav-open');
+  $('header .navbar').fadeToggle(200);
+    });
+
+  $("header li a").click(function(){
   $('body').toggleClass('nav-open');
   $('header .navbar').fadeToggle(200);
     });
